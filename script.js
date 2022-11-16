9ebe96e038e4b6a2b395ef45fd8ad59687e1be55
Nicholas <NLoo1@outlook.com>
2022-11-16 01:00:40 -0500

    Cleaned up code. Resizing not working on main

--- script.js
+++ script.js
@@ -37,36 +37,17 @@ function newSize(){
     let slotX = 400/sizeX;
     let slotY = 400/sizeY;
 
-    // console.log(slotX + " SLOTX")
+    let x = document.styleSheets[0];
+    x.insertRule('.grid-slot{width: slotX; height:slotY', 0)
 
-    // let newSize = document.querySelector(".grid-slot");
-    // newSize.style.width = slotX +  "px";
-    // newSize.style.height = slotY + "px";
-
-    // console.log(newSize.style.width + " NEW WIDTH")
-
-    // let change = document.getElementsByClassName('grid-slot')
-    // for(let i = 0; i < change.length; i++){
-    //     change[i].style.width = slotX+"px";
-    //     change[i].style.height = slotY+"px";
-    // }
-    // change.width = 400/sizeX;
-    // change.height = 400/sizeY;
 
     document.getElementById("grid-container").innerHTML = "";
-    // let newGrid = document.getElementById('grid-container');
-    // // // grid-template-rows: repeat(16, 25px);
-    // // // grid-template-columns: repeat(16, 25px);
-    // newGrid.style.setProperty('grid-template-rows', 'repeat(' + sizeX + ', slotX+"px")');
-    // newGrid.style.setProperty('grid-template-columns', 'repeat(' + sizeY + ', slotY+"px")');
+
 
     for(i = 0; i < (sizeX*sizeY); i++){
         let d = document.createElement('div');
         d.className = "grid-slot";
  
-        // console.log(d.style.width)
-
-        // d.textContent = '';
         document.getElementById('grid-container').append(d);
     }
 
